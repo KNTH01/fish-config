668dcfbeb836fd35d88104cc6a90dec1ab1f4878
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2016-03-16 16:05:23 +0100

    remove loading files which don't exist

--- config.fish
+++ config.fish
@@ -1,5 +1,3 @@
 # need a way to auto load these
-. "$HOME/.config/fish/functions/export.fish"
 . "$HOME/.config/fish/functions/aliases.fish"
 . "$HOME/.config/fish/functions/aliases_ck.fish"
-. "$HOME/.config/fish/functions/utils.fish"
